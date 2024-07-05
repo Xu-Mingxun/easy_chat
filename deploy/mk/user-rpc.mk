a3d32d4e6c8d90d17b2b76389037bdbb157740a0
Xu Mingxun <mingxunxu00@gmail.com>
2024-07-06 01:09:07 +0800

    deployment succeed

--- deploy/mk/user-rpc.mk
+++ deploy/mk/user-rpc.mk
@@ -6,7 +6,7 @@ SERVER_TYPE=rpc
 
 # 测试环境配置
 # docker的镜像发布地址
-DOCKER_REPO_TEST=registry.cn-hangzhou.aliyuncs.com/easy-chat/${SERVER_NAME}-${SERVER_TYPE}-dev
+DOCKER_REPO_TEST=registry.cn-hangzhou.aliyuncs.com/easy-chat-xu/${SERVER_NAME}-${SERVER_TYPE}-dev
 
 # 测试版本
 VERSION_TEST=$(VERSION)
